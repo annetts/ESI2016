d4e2f23c6c12750530d164b9f34a4f6583379d8a
Rain <rain@Rains-MacBook-Pro.local>
2016-02-19 05:41:25 +0200

    Added Postres db support

--- src/main/java/com/example/controllers/PlantsController.java
+++ src/main/java/com/example/controllers/PlantsController.java
@@ -1,7 +1,7 @@
 package com.example.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.models.Plant;
 import com.example.models.PlantRepository;
 
+@Controller
 public class PlantsController {
 
 	@Autowired
